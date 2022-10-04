d11869d29bc4f26a9da7b165e8dedb14bd132e9d
Jordan Bean <jorbea@live.unc.edu>
2022-10-03 23:58:52 -0400

    Added basics of fetch, need to accommodate negative sign for lat and long

--- cli.js
+++ cli.js
@@ -3,6 +3,7 @@
 // Dependencies
 import minimist from 'minimist';
 const args = minimist(process.argv.slice(2));
+console.log(args);
 
 // Help Message
 if (args.h) {
@@ -17,3 +18,46 @@ if (args.h) {
 	`);
 	process.exit(0);
 };
+
+// Timezone
+import moment from 'moment-timezone';
+const timezone = moment.tz.guess();
+console.log(timezone);
+
+// Latitude & Longitude
+let latitude = null;
+let longitude = null;
+if (args['n']) {
+	latitude = args['n'];
+	console.log('-n ' + latitude);
+} else if (args['s']) {
+	latitude = args['s'];
+	console.log('-s ' + latitude);
+}
+if (args['e']) {
+	longitude = args['e'];
+	console.log('-e ' + longitude);
+} else if (args['w']) {
+	longitude = args['w'];
+	console.log('-w ' + longitude);
+}
+
+// Fetch
+import fetch from 'node-fetch';
+// const response = await fetch(
+	// 'https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + 
+	// '&daily=precipitation_hours&timezone=America%2FNew_York'
+// );
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=precipitation_hours&timezone=America%2FNew_York');
+const data = await response.json();
+console.log(data);
+
+// Response
+const days = args.d; 
+if (days == 0) {
+  console.log("today.");
+} else if (days > 1) {
+  console.log("in " + days + " days.");
+} else {
+  console.log("tomorrow.");
+}
